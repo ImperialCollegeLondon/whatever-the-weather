a427a21d5ea201bcac5460aeaa51c251f4ec262a
Srayan Gangopadhyay <sgango@users.noreply.github.com>
2020-07-23 13:14:54 +0100

    refactor code and remove print statements

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
-import getpass
-import json
-import os
-import smtplib
-import ssl
-import urllib.request
-import geopy
+"""
+Author: Srayan Gangopadhyay
+2020-07-23
+Adapted from MIT-licensed code by Mark Woodbridge
+"""
+
+import getpass  # for secure password retrieval
+import json  # convert json to python dict
+import os  # access environment variables
+import smtplib  # send emails
+import ssl  # network stuff
+import urllib.request  # fetch file from url
+import geopy  # turn location into lat/long
 from geopy.geocoders import Nominatim
 
-email = os.environ["EMAIL"] if "EMAIL" in os.environ else input("Log in to send an email.\nEmail: ")
-password = os.environ["PASSWORD"] if "PASSWORD" in os.environ else getpass.getpass(prompt='Password: ')
-locationstring = os.environ["LOCATION"] if "LOCATION" in os.environ else input("Location: ")
+def env_setup():
+    """Get parameters from environment variables,
+    else get them from stdin.
+    """
+    # email-sending parameters
+    email = (os.environ["EMAIL"] if "EMAIL" in os.environ 
+        else input("Log in to send an email.\nEmail: "))
+    password = (os.environ["PASSWORD"] if "PASSWORD" in os.environ 
+        else getpass.getpass(prompt='Password: '))
+    recipient = (os.environ["RECIPIENT"] if "RECIPIENT" in os.environ 
+        else input("Recipient email: "))
+    # convert location to lat-long
+    locationstring = (os.environ["LOCATION"] if "LOCATION" in os.environ 
+        else input("Location: "))
+    locator = Nominatim(user_agent="myGeocoder")
+    loc = locator.geocode(locationstring)
+    return email, password, recipient, loc
 
-locator = Nominatim(user_agent="myGeocoder")
-location = locator.geocode(locationstring)
+def get_weather(loc):
+    """Fetch weather data from Meteorologisk Institutt."""
+    url = (f"https://api.met.no/weatherapi/locationforecast/"
+        f"2.0/compact?lat={loc.latitude}&lon={loc.longitude}")
+    with urllib.request.urlopen(url) as fp:
+        forecast = json.load(fp)
+        precipitation = (forecast["properties"]["timeseries"][0]["data"]
+            ["next_6_hours"]["details"]["precipitation_amount"])
+        return precipitation
 
-print("Fetching weather for: ",location.address)
-print("Latitude = {}, Longitude = {}".format(location.latitude, location.longitude))
+def send_email(precipitation, loc, email, password, recipient):
+    """Log into server and send an email with precipitation forecast."""
+    msg = (f"Subject: Don\'t forget your umbrella!\n"
+        f"\n{precipitation}mm of precipitation is forecast in {loc.address}.")
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", context=context) as server:
+        server.login(email, password)
+        server.sendmail(email, recipient, msg)
 
-with urllib.request.urlopen(f"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={location.latitude}&lon={location.longitude}") as fp:
-    forecast = json.load(fp)
-    precipitation = forecast["properties"]["timeseries"][0]["data"]["next_6_hours"]["details"]["precipitation_amount"]
-
-    print(f"{precipitation}mm of precipitation is forecast.")
-
+if __name__ == "__main__":
+    email, password, recipient, location = env_setup()
+    precipitation = get_weather(location)
     if precipitation > 0:
-        msg = f"Subject: Don't forget your umbrella!\n\n{precipitation}mm of precipitation is forecast in {location.address}."
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", context=context) as server:
-            server.login(email, password)
-            server.sendmail(email, email, msg)
-        print("Email sent.")
-    else:
-        print("No email sent.")
+        send_email(precipitation, location, email, password, recipient)
